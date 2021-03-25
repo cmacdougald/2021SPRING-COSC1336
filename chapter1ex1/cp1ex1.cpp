fcc1699d66137d8f3d662dba79c0bac164b30518
Chris MacDougald <cmacdougald@alamo.edu>
2021-03-25 18:49:10 -0500

    Add files via upload

--- chapter1ex1/cp1ex1.cpp
+++ chapter1ex1/cp1ex1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
+// Main Function Start of Program
 int main() {
     std::cout << "Chapter 1, Example 1" << std::endl; 
+    std::cout << "I got help with this line" << std::endl;
     return 0;
 }
