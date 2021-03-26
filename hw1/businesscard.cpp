019204de78e674cfa4f60ca364012d7c77f2db3b
Chris MacDougald <cmacdougald@alamo.edu>
2021-03-25 19:40:44 -0500

    Add files via upload

---
+++ hw1/businesscard.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Sushishima Japanese Restaurant" << std::endl;
+    std::cout << "2211 NW Military Hwy Suite 132, Castle Hills, TX 78213" << std::endl;
+    std::cout << "+12108880363" << std::endl;
+    std::cout << "http://sushishima.com/" << std::endl;
+    return 0;
+}
