c22f0dc3f58018422f684d8a6bd9cb8d421a961c
Chris MacDougald <cmacdougald@alamo.edu>
2021-03-30 19:13:04 -0500

    Add files via upload

---
+++ chapter3ex1/chapter3ex1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Output First" << std::endl;
+    std::cout << "Input Second" << std::endl;
+    std::cout << "Processing Finally" << std::endl;
+}
